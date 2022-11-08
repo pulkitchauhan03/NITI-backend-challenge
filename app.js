d3a55447289d575f809d693d60b456527558794d
pulkitchauhan03 <53511622+pulkitchauhan03@users.noreply.github.com>
2022-11-09 03:42:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const consentPage = fs.readFileSync(
 
 const config = {
     headers: {
-        'x-client-id': '255d0b6c-492d-44cf-8581-e9494c7b0914',
-        'x-client-secret': '9c2fc756-3d17-46d6-b28d-be4d71953e83',
+        'x-client-id': process.env.CLIENT_ID,
+        'x-client-secret': process.env.CLIENT_SECRET,
     },
 };
 
